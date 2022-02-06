832c3755e9dc2f52c38e68cab83441366a97fe29
meSajied <sajiedshahyousuf@gmail.com>
2022-02-06 14:03:12 +0600

    add: fetching data using useReducer

--- components/ReducerHook.js
+++ components/ReducerHook.js
@@ -1,6 +1,6 @@
 // code is imported from github.com/gopinav
 
-import React, {useReducer} from 'react'
+import React, {useEffect, useReducer} from 'react'
 
 const initialState = 0
 const reducer = (state, action) => {
@@ -16,7 +16,7 @@ const reducer = (state, action) => {
 	}
 }
 
-function ReducerHook() {
+export function ReducerHook() {
 	const [count, dispatch] = useReducer(reducer, initialState)
 	const [countTwo, dispatchTwo] = useReducer(reducer, initialState)
 
@@ -35,4 +35,48 @@ function ReducerHook() {
 	)
 }
 
-export default ReducerHook
+const initialStateReducer = {
+	loading: true,
+	error: '',
+	post: {}
+}
+
+const fetchReducer = (state, action) => {
+	switch (action.type) {
+		case 'FETCH_SUCCESS':
+			return {
+				loading: false,
+				post: action.payload,
+				error: ''
+			}
+		case 'FETCH_ERROR':
+			return {
+				loading: false,
+				post: {},
+				error: 'Something went wrong!'
+			}
+		default:
+			return state
+	}
+}
+
+export function DataFetchingWithUseReducer() {
+	const [state, dispatch] = useReducer(fetchReducer, initialStateReducer)
+
+	useEffect(() => {
+		axios
+				.get(`https://jsonplaceholder.typicode.com/posts/1`)
+				.then(response => {
+					dispatch({type: 'FETCH_SUCCESS', payload: response.data})
+				})
+				.catch(error => {
+					dispatch({type: 'FETCH_ERROR'})
+				})
+	}, [])
+	return (
+			<div>
+				{state.loading ? 'Loading' : state.post.title}
+				{state.error ? state.error : null}
+			</div>
+	)
+}
